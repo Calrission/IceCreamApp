8b5b57cc4281ffa212842ecb1f3165c6d3f34690
Kavrese <va2005art@gmail.com>
2022-02-06 15:05:48 +0300

    Add bottom sheet behavior in MapsScreen

--- app/src/main/java/com/ege/icecreamapp/MapScreen/MapsActivity.kt
+++ app/src/main/java/com/ege/icecreamapp/MapScreen/MapsActivity.kt
@@ -2,6 +2,9 @@ package com.ege.icecreamapp.MapScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.LinearLayout
 import com.ege.icecreamapp.R
 
 import com.google.android.gms.maps.CameraUpdateFactory
@@ -11,12 +14,16 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import com.ege.icecreamapp.databinding.ActivityMapsBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetBehavior.*
 import kotlinx.android.synthetic.main.activity_maps.*
+import kotlinx.android.synthetic.main.bottom_sheet_map_screen.*
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var bottomSheetBehavior: BottomSheetBehavior<LinearLayout>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +32,8 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         val mapFragment = supportFragmentManager
             .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+
+        initBottomSheetBehavior()
 
         back.setOnClickListener {
             finish()
@@ -35,5 +44,23 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
         val moscow = LatLng(55.7522, 37.6156)
         mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(moscow, 10f))
+    }
+
+    private fun initBottomSheetBehavior() {
+        bottomSheetBehavior = BottomSheetBehavior.from(bottom_sheet_layout)
+        hide_bottom_sheet.setOnClickListener {
+            bottomSheetBehavior.state = STATE_COLLAPSED
+        }
+        bottomSheetBehavior.state = STATE_EXPANDED
+        bottomSheetBehavior.addBottomSheetCallback(object:
+            BottomSheetBehavior.BottomSheetCallback() {
+            override fun onStateChanged(bottomSheet: View, newState: Int) {
+                hide_bottom_sheet.visibility = if (newState == STATE_COLLAPSED) View.GONE else View.VISIBLE
+            }
+
+            override fun onSlide(bottomSheet: View, slideOffset: Float) {
+                hide_bottom_sheet.alpha = slideOffset
+            }
+        })
     }
 }
